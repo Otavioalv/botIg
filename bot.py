14982aacdabf1da6fc214377b86adfd6cc1a2532
Otavioalv <otaviogabriel.alves.barbosa@gmail.com>
2024-07-08 11:33:33 -0400

    updates

--- bot.py
+++ bot.py
@@ -42,42 +42,44 @@ class InstagramBot:
     def button_follower(self):
         driver = self.driver
         try:
-            button_list = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "//ul[contains(@class, 'x78zum5') and contains(@class, 'x1q0g3np') and contains(@class, 'xieb3on')]"))
+            # Espera até que o elemento <a> com href '/amazon/followers/' esteja presente
+            button = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//a[contains(@href, '/amazon/followers/')]"))
             )
-            print(button_list)
+            driver.execute_script('arguments[0].click();', button)
+            sleep(5)
         except Exception as e:
             print(f"An error occurred: {e}")
-            
-    def button_followers(self):
-        driver = self.driver
-        # x78zum5 x1q0g3np xieb3on
-        button_list = driver.find_element(By.XPATH, "//ul[contains(@class, 'x78zum5') and contains(@class, 'x1q0g3np') and contains(@class, 'xieb3on')]")
-        print(button_list)
+
         
     def follow_all(self):
         driver = self.driver
         
-        # _acan _acap _acas _aj1- _ap30
-        button_list = driver.find_elements(By.XPATH, "//button[contains(@class, '_acan') and contains(@class, '_acap') and contains(@class, '_acas') and contains(@class, '_aj1-') and contains(@class, '_ap30)]")
+        try:
+            # XPath corrigido para buscar o botão com as classes especificadas
+            button_list = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.XPATH, "//button[contains(@class, '_acan') and contains(@class, '_acap') and contains(@class, '_acas') and contains(@class, '_aj1-') and contains(@class, '_ap30')]"))
+            )
 
-        seguidor = 0
-        teste = 0
-            
-        while teste < 10:
-            for element in button_list: 
-                driver.execute_script('arguments[0].click();', element)
-                seguidor += 1
-                
-                print(seguidor)
-                sleep(7)
-            teste+=1
+            seguidor = 0
+            teste = 0
+
+            while teste < 10:
+                for element in button_list:
+                    driver.execute_script('arguments[0].click();', element)
+                    seguidor += 1
+                    print(seguidor)
+                    sleep(7)
+                teste += 1
+        except Exception as e:
+            print(f"An error occurred: {e}")
         
  
 Bot = InstagramBot('otavioalv_1', 'galego00');
 Bot.login();
 Bot.find_profile('amazon')
-Bot.button_followers()
+Bot.button_follower()
+Bot.follow_all()
 
         
         
